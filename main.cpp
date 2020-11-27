8382baf968a39cc8c79c43a1e791452d8718583c
Hongbin Miao <59853402+zslmrhb@users.noreply.github.com>
2020-11-27 12:41:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,23 @@ using namespace cv;
 
 int main()
 {
-    VideoCapture cap("/home/einstein/桌面/wind.mp4");
+    VideoCapture cap("/home/einstein/桌面/wind.mp4");  // 桌面: home
     Mat image,binary;
     int stateNum = 4;
     int measureNum = 2;
     KalmanFilter KF(stateNum, measureNum, 0);
     //Mat processNoise(stateNum, 1, CV_32F);
     Mat measurement = Mat::zeros(measureNum, 1, CV_32F);
-    KF.transitionMatrix = (Mat_<float>(stateNum, stateNum) << 1, 0, 1, 0,//A 状态转移矩阵
+    KF.transitionMatrix = (Mat_<float>(stateNum, stateNum) << 1, 0, 1, 0,//A 状态转移矩阵   A  status transition matrix
         0, 1, 0, 1,
         0, 0, 1, 0,
         0, 0, 0, 1);
-    //这里没有设置控制矩阵B，默认为零
-    setIdentity(KF.measurementMatrix);//H=[1,0,0,0;0,1,0,0] 测量矩阵
-    setIdentity(KF.processNoiseCov, Scalar::all(1e-5));//Q高斯白噪声，单位阵
-    setIdentity(KF.measurementNoiseCov, Scalar::all(1e-1));//R高斯白噪声，单位阵
-    setIdentity(KF.errorCovPost, Scalar::all(1));//P后验误差估计协方差矩阵，初始化为单位阵
-    randn(KF.statePost, Scalar::all(0), Scalar::all(0.1));//初始化状态为随机值
+    //这里没有设置控制矩阵B，默认为零     did not set a control matrix B, default to 0.     
+    setIdentity(KF.measurementMatrix);//H=[1,0,0,0;0,1,0,0] 测量矩阵    measurement matrix
+    setIdentity(KF.processNoiseCov, Scalar::all(1e-5));//Q高斯白噪声，单位阵   Q White Gaussian noise, identity matrix 
+    setIdentity(KF.measurementNoiseCov, Scalar::all(1e-1));//R高斯白噪声，单位阵 R White Guassian noise, identity matrix
+    setIdentity(KF.errorCovPost, Scalar::all(1));//P后验误差估计协方差矩阵，初始化为单位阵  P Posterior error estimation covariance matrix, initialized to an identity matrix
+    randn(KF.statePost, Scalar::all(0), Scalar::all(0.1));//初始化状态为随机值   Initial state --> randomized
 
     for(;;){
         cap.read(image);
@@ -36,7 +36,7 @@ int main()
 
         cvtColor(image,image,COLOR_BGR2GRAY);
 
-        threshold(image, image, 80, 255, THRESH_BINARY);        //阈值要自己调
+        threshold(image, image, 80, 255, THRESH_BINARY);        //阈值要自己调    Set the threshold manually 
 
         dilate(image,image,Mat());
         dilate(image,image,Mat());
@@ -87,7 +87,7 @@ int main()
                         middle = distance;
                     }
                 }
-                if( middle > 60){                               //这个距离也要根据实际情况调,和图像尺寸和物体远近有关。
+                if( middle > 60){                               //这个距离也要根据实际情况调,和图像尺寸和物体远近有关。set this distance depends on your situation, it's associated with the image size and the distance between objects
                     cv::circle(binary,Point(rrect.center.x,rrect.center.y),15,cv::Scalar(0,0,255),4);
                     Mat prediction = KF.predict();
                     Point predict_pt = Point((int)prediction.at<float>(0), (int)prediction.at<float>(1));
